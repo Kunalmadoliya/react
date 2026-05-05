c6fa59f562c22597a061bf4542c5cbcf056de60a
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-05-05 16:17:56 +0530

    done

--- 06-project/src/App.tsx
+++ 06-project/src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState, useMemo} from "react";
+import {useEffect, useState} from "react";
 import {motion, AnimatePresence} from "framer-motion";
 
 export default function StreamInterface() {
--- Product Page/src/App.tsx
+++ Product Page/src/App.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect, useMemo} from "react";
+import {useState, useEffect,useMemo} from "react";
 import {motion, AnimatePresence} from "framer-motion";
 
 export default function SystemMarketplace() {
